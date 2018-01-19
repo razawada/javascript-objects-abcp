69dc3e2ec1e086603a625f61c5035bdebc1656ff
Rance Zawada <razawada@gmail.com>
2018-01-19 18:40:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,4 +8,11 @@ function updatePlaylist(obj, key, value) {
   return obj;
 };
 
+function removeFromPlaylist(obj, key) {
+  delete obj.key;
+  return obj;
+};
+
 updatePlaylist(playlist, 'Gaga', 'Bad Romance');
+
+removeFromPlaylist(playlist, "Gaga");
