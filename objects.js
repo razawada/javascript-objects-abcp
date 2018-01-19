7d28fd8638461782f27601ce28ec4a8a9a97147e
Rance Zawada <razawada@gmail.com>
2018-01-19 18:29:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,11 @@
 var playlist = {
   Marina: "Blue";
-}
+};
+
+function updatePlaylist(obj, key, value) {
+  obj[key] = value;
+
+  return obj;
+};
+
+updatePlaylist(playlist, "Gaga", "Bad Romance");
