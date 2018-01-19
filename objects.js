4a8ff88ba1446c3b60f2a1ddc953436f0c309799
Rance Zawada <razawada@gmail.com>
2018-01-19 18:17:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,3 @@
-var playist = { Marina: "Blue" };
-
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle;
-
-  return playlist;
-}
-
-function removeFromPlaylist(key, value) {
-
+var playlist = {
+  Marina: "Blue";
 }
